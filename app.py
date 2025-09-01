0e018ae8ac734b64fd3bdf28a8e794088dd23f57
Prasanna Madiwar <prasannamadiwar71@gmail.com>
2025-09-01 22:41:55 +0530

    To coreect the spelling of requirements.txt

--- app.py
+++ app.py
@@ -170,4 +170,5 @@ def predict():
         return f"Error processing prediction: {str(e)}"
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Heroku/Dokku gives a PORT
+    app.run(host="0.0.0.0", port=port)
